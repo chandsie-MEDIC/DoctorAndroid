4df37f1185c16c52ded14a5617e921ca1b8e04c4
Bri Mussman <bri.mussman@gmail.com>
2012-04-09 21:58:58 -0400

    initial commit, autogenerted files.

---
+++ src/com/shreyaschand/MEDIC/Doctor/MEDIC.java
@@ -0,0 +1,13 @@
+package com.shreyaschand.MEDIC.Doctor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MEDIC extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
