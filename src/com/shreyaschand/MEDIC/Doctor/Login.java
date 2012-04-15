211c6e473e90b59b0ec1fc67ad2f63d4cbc1c87d
Shreyas Chand <shreyas.chand@gmail.com>
2012-04-15 01:45:16 -0400

    Added user authentication.

--- src/com/shreyaschand/MEDIC/Doctor/Login.java
+++ src/com/shreyaschand/MEDIC/Doctor/Login.java
@@ -1,5 +1,13 @@
 package com.shreyaschand.MEDIC.Doctor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,9 +28,25 @@ public class Login extends Activity implements OnClickListener {
 		String pword = ((EditText)findViewById(R.id.login_password)).getText().toString();
 
 		Toast.makeText(this, "Logging In...", Toast.LENGTH_SHORT).show();
-		 
-		//Do stuff with uname and pword
-		//Call next activity
-		
+		String response = "Error";
+		try {
+			String data = URLEncoder.encode("username", "UTF-8") + "=" + URLEncoder.encode(uname, "UTF-8") + "&" 
+						+ URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(pword, "UTF-8");
+			URLConnection conn = new URL("http://chands.dyndns-server.com:4080/auth/").openConnection();
+			conn.setDoOutput(true);
+			OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+			wr.write(data);
+			wr.flush();
+			wr.close();
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream())); 
+			response = br.readLine();
+			br.close();
+		} catch (IOException e) {e.printStackTrace();Toast.makeText(this, "Connection Error. Try Again Later.", Toast.LENGTH_SHORT).show();}
+		if(response.equals("Error")) {
+			Toast.makeText(this, "Incorrect username or password.", Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(this, "Welcome, " + response, Toast.LENGTH_SHORT).show();
+			//Call next activity
+		}
 	}
 }
